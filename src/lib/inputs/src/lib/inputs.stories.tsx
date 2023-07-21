7e9d7caa5b46d563487acf9c3a6e9ab0737f9710
pjnalls <42269808+pjnalls@users.noreply.github.com>
2023-07-21 14:49:27 -0500

    Generate stories for `src/lib/data-grid` library with Nx Console.

---
+++ src/lib/inputs/src/lib/inputs.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { Inputs } from './inputs';
+
+const Story: Meta<typeof Inputs> = {
+  component: Inputs,
+  title: 'Inputs',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
